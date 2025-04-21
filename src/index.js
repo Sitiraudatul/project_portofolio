2804559646873af302bd6799837e4270844f7d17
Sitiraudatul <sitiraudatuljannahfadilah@gmail.com>
2025-04-21 23:15:57 +0700

    update buat deploy

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { HashRouter as Router } from "react-router-dom"; // Menambahkan HashRouter
+import { BrowserRouter } from "react-router-dom"; // Ubah ke BrowserRouter
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <Router>  {/* Mengganti BrowserRouter dengan HashRouter */}
-            <App />
-        </Router>
-    </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter basename="/project_portofolio">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
